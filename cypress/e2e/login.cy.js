30efac0a2990691b2aad630e7cfd1081da3a724b
pereiralim <v.ipereira17.vp@gmail.com>
2025-04-23 18:07:03 -0300

    meu primeiro commit

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,43 @@
+///<reference types="cypress"/>
+
+
+describe("Funcionalidade de Login", () => {
+    context('Quando o usuario e valido', () => {
+
+        it('Deve fazer login com sucesso', () => {
+
+            cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+            cy.get('#username').type('aluno_ebac@teste.com')
+            cy.get('#password').type('teste@teste.com')
+            cy.get('.woocommerce-form > .button').click()
+
+            cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá,')
+            cy.get('a > .hidden-xs').should('contain', 'Welcome')
+        })
+    })
+
+    context('Quando o usuario e invalido', () => {
+        it('Deve exibir uma mensagem de erro ao inserir usuario invalido', () => {
+
+            cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+            cy.get('#username').type('aluno_ebac@teste')
+            cy.get('#password').type('teste@teste.com')
+            cy.get('.woocommerce-form > .button').click()
+    
+            cy.get('.woocommerce-error').should('contain', 'Erro:')
+        })
+    })
+    
+    context('Quando a senha do usuario e invalida', () => {
+        it('Deve exibir uma mensagem de erro ao inserir senha invalida', () => {
+
+            cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+            cy.get('#username').type('aluno_ebac@teste.com')
+            cy.get('#password').type('teste@teste')
+            cy.get('.woocommerce-form > .button').click()
+
+            cy.get('.woocommerce-error').should('contain', 'Perdeu a senha?')
+        })
+    })
+    
+})
